8e13d0819c34d5a85b8c05d4e62af5157b6ee870
hamza15786 <43768836+hamza15786@users.noreply.github.com>
2021-05-19 03:56:28 -0500

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,14 +1,14 @@
 module "dev-compute" {
-  source = "./multiple_envs_tfc/modules/compute"
+  source = "./modules/compute"
   count = var.num_instances
 }
 
 module "dev-firewall" {
-  source = "./multiple_envs_tfc/modules/firewall"
+  source = "./modules/firewall"
   networks = var.networks
 }
 
 module "dev-network" {
-  source = "./multiple_envs_tfc/modules/network"
+  source = "./modules/network"
   ports = var.list_ports
 }
